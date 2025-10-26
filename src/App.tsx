abbe8d9556c72a12ab30f396094260a3a89a9993
aria_bz <ariabahmanzangi@gmail.com>
2025-10-26 18:52:22 +0330

    v2

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,14 @@ import Card from "./components/Card";
 
 function App() {
   return (
-    <div className=" flex flex-col justify-center items-center h-full gap-6">
+    <div className=" flex flex-col justify-center items-center h-full gap-8">
       <span id="title" className="text-[#c59d5f] text-4xl">
         general questions
       </span>
-      <div>
-        <div></div>
-        <div></div>
-        <div></div>
+      <div className="flex flex-col gap-7">
+        <Card title="do you accept all major credit cards?"></Card>
+        <Card title="do you suppport local farmers?"></Card>
+        <Card title="ddo you use organic ingredients?"></Card>
       </div>
     </div>
   );
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,22 +1,36 @@
 import { useState } from "react";
 
 interface CardPropsType {
-  title1: "do you accept all major credit cards?";
+  title: string;
 }
 
-function Card({ title1 }: CardPropsType) {
-  const [open, setopen] = useState("+");
+function Card({ title }: CardPropsType) {
+  let [open, setopen] = useState("+");
   return (
-    <div>
-      <div>
-        <span>sss</span>
-        <button>s</button>
+    <div className="w-[400px] max-w-[620px] bg-white shadow-xl rounded p-5 transition-all duration-300">
+      <div className="flex justify-between items-center">
+        <span className="capitalize text-slate-800 font-medium">{title}</span>
+        <button
+          className="hover:scale-90 cursor-pointer transition-transform text-xl pb-1 border-2 border-[#c59d5f] text-[#c59d5f] flex justify-center items-center w-7  h-7"
+          onClick={() => {
+            if (open === "+") {
+              setopen("-");
+            } else {
+              setopen("+");
+            }
+          }}
+        >
+          {open}
+        </button>
       </div>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est dolore
-        illo dolores quia nemo doloribus quaerat, magni numquam repellat
-        reprehenderit.
-      </p>
+      <div id="text" className="hidetext mt-4 flex flex-col gap-4">
+        <hr className="text-gray-500" />
+        <p className="text-[#617d98] text-sm">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est dolore
+          illo dolores quia nemo doloribus quaerat, magni numquam repellat
+          reprehenderit.
+        </p>
+      </div>
     </div>
   );
 }
